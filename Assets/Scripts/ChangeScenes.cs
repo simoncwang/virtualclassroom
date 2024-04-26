3fa32834d5b9ca63c57a70c7427922e50f624858
genevieve-tech <jang.genevieve@gmail.com>
2024-04-26 14:51:40 -0400

    organized virtual classroom scene

--- Assets/Scripts/ChangeScenes.cs
+++ Assets/Scripts/ChangeScenes.cs
@@ -6,13 +6,11 @@ using UnityEngine.SceneManagement;
 
 public class ChangeScenes : MonoBehaviour
 {
-    // Start is called before the first frame update
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
         
