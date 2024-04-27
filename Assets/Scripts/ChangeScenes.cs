6e72c0e5da106933b1eb7572f41b5b100cee047a
genevieve-tech <jang.genevieve@gmail.com>
2024-04-26 21:24:45 -0400

    added controller prefab + default ray cast (color: white-ish)

--- Assets/Scripts/ChangeScenes.cs
+++ Assets/Scripts/ChangeScenes.cs
@@ -1,28 +1,83 @@
-// using System.Collections;
-// using System.Collections.Generic;
-// using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
-// using UnityEngine.SceneManagement;
+using UnityEngine.SceneManagement;
 
-// public class ChangeScenes : MonoBehaviour
-// {
-//     public string sceneName;
+public class ChangeScenes : MonoBehaviour
+{
+    LineRenderer lineRenderer;
+    public string sceneName;
+    private float maxRayDistance = 15f;
 
-//     void OnTriggerEnter(Collider other)
-//     {
-//         if (other.CompareTag("Next"))
-//         {
-//             SceneManager.LoadScene(sceneName);
-//         }
-//     }
+    // void OnTriggerEnter(Collider other)
+    // {
+    //     if (other.CompareTag("Next"))
+    //     {
+    //         SceneManager.LoadScene(sceneName);
+    //     }
+    // }
 
-//     void Start()
-//     {
-        
-//     }
+    void Start()
+    {
+        lineRenderer = GetComponent<LineRenderer>();
+    }
 
-//     void Update()
-//     {
-        
-//     }
-// }
+    void Update()
+    {
+        RenderRay(); // render red ray at all times
+    
+        // if (GetButtonPress())
+        // {
+        //     CastRay();
+        // }
+
+        // if (GetButtonRelease())
+        // {
+        //     lineRenderer.enabled = false;
+        // }
+    }
+
+    bool GetButtonPress()
+    {
+        return OVRInput.GetDown(OVRInput.Button.One);
+    }
+
+    bool GetButtonRelease()
+    {
+        return OVRInput.GetUp(OVRInput.Button.One);
+    }
+
+    void RenderRay()
+    {
+        Debug.Log("Controller position: " + transform.position + ", Controller forward: " + transform.forward.normalized);
+        lineRenderer.SetPosition(0, transform.position);
+        lineRenderer.SetPosition(1, transform.forward * maxRayDistance);
+        lineRenderer.enabled = true;
+    }
+
+    void SetRayColor()
+    {
+
+    }
+
+    // void CastRay()
+    // {
+    //     RaycastHit hit;
+    //     if (Physics.Raycast(transform.position, transform.forward.normalized, out hit, maxRayDistance))
+    //     {
+    //         GameObject hitObject = hit.collider.gameObject;
+    //         Debug.Log("Ray hit [" + hitObject + "]");
+    //         if (hitObject.CompareTag("Next"))
+    //         {
+    //             lineRenderer.SetPosition(1, hitObject.transform.position);
+    //             lineRenderer.enabled = true;
+
+    //             Debug.Log("Loading next scene...");
+    //             SceneManager.LoadScene(sceneName);
+    //         }
+    //     } else {
+    //         Debug.Log("Ray hit nothing.");
+    //     }
+    // }
+}
