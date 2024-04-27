8b585f7ea1848fe5f06efe5e845d82762f986601
genevieve-tech <jang.genevieve@gmail.com>
2024-04-26 22:54:28 -0400

    ray cast changes to green upon hitting arrow ('Next') button; coordinates are off though

--- Assets/Scripts/ChangeScenes.cs
+++ Assets/Scripts/ChangeScenes.cs
@@ -7,8 +7,13 @@ using UnityEngine.SceneManagement;
 public class ChangeScenes : MonoBehaviour
 {
     LineRenderer lineRenderer;
+    GameObject hitObject;
+    private float maxRayDistance = 20f;
+
+    public Material defaultPointerMaterial; // maybe there's a better way to do this...?
+    public Material selectPointerMaterial;
+
     public string sceneName;
-    private float maxRayDistance = 15f;
 
     // void OnTriggerEnter(Collider other)
     // {
@@ -21,20 +26,21 @@ public class ChangeScenes : MonoBehaviour
     void Start()
     {
         lineRenderer = GetComponent<LineRenderer>();
+        SetRayMaterial(lineRenderer, defaultPointerMaterial);
     }
 
     void Update()
     {
-        RenderRay(); // render red ray at all times
-    
-        // if (GetButtonPress())
-        // {
-        //     CastRay();
-        // }
+        CastRay();
+
+        if (GetButtonPress())
+        {
+            // ChangeScene(); // change scene upon button press
+        }
 
         // if (GetButtonRelease())
         // {
-        //     lineRenderer.enabled = false;
+            
         // }
     }
 
@@ -50,34 +56,43 @@ public class ChangeScenes : MonoBehaviour
 
     void RenderRay()
     {
-        Debug.Log("Controller position: " + transform.position + ", Controller forward: " + transform.forward.normalized);
+        // Debug.Log("Controller position: " + transform.position + ", Controller forward: " + transform.forward.normalized);
         lineRenderer.SetPosition(0, transform.position);
         lineRenderer.SetPosition(1, transform.forward * maxRayDistance);
         lineRenderer.enabled = true;
     }
 
-    void SetRayColor()
+    void CastRay()
     {
+        RenderRay(); // render default (white) ray at all times
 
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, transform.forward.normalized, out hit, maxRayDistance))
+        {
+            hitObject = hit.collider.gameObject;
+            Debug.Log("Ray hit [" + hitObject + "]");
+            if (hitObject.name.StartsWith("Arrow")) // alternatively, .CompareTag("Next")
+            {
+                SetRayMaterial(lineRenderer, selectPointerMaterial);
+            } else {
+                SetRayMaterial(lineRenderer, defaultPointerMaterial); // change cast ray color back to default color
+            }
+        } else {
+            Debug.Log("Ray hit nothing.");
+        }
     }
 
-    // void CastRay()
-    // {
-    //     RaycastHit hit;
-    //     if (Physics.Raycast(transform.position, transform.forward.normalized, out hit, maxRayDistance))
-    //     {
-    //         GameObject hitObject = hit.collider.gameObject;
-    //         Debug.Log("Ray hit [" + hitObject + "]");
-    //         if (hitObject.CompareTag("Next"))
-    //         {
-    //             lineRenderer.SetPosition(1, hitObject.transform.position);
-    //             lineRenderer.enabled = true;
+    // load next scene
+    void ChangeScene()
+    {
+        Debug.Log("Loading next scene...");
+        SceneManager.LoadScene(sceneName);        
+    }
 
-    //             Debug.Log("Loading next scene...");
-    //             SceneManager.LoadScene(sceneName);
-    //         }
-    //     } else {
-    //         Debug.Log("Ray hit nothing.");
-    //     }
-    // }
+    // change ray cast color to green upon hitting Next button
+    // (though ig I could have changed just the color instead of the entire material...)
+    void SetRayMaterial(LineRenderer LineRenderer, Material material)
+    {
+        LineRenderer.material = material;
+    }
 }
