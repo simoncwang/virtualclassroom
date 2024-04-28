0f0a1b024145ebd91bc30162b04a8d1a50fd9ac9
simoncwang <wang.c.simon@gmail.com>
2024-04-28 01:10:34 -0400

    changed rotation to be in local coordinates

--- Assets/Scripts/RotateObject.cs
+++ Assets/Scripts/RotateObject.cs
@@ -16,11 +16,15 @@ public class RotateObject : MonoBehaviour
     }
 
     public void rotateObject(GameObject objectToRotate, Vector3 hitPosition) {
-        // NOTE: vector from origin to hit point is just hit position - (0,0,0)
+        // NOTE: vector from origin to hit point is just hit position - (0,0,0) which is hit position
         Debug.Log(hitPosition);
 
-        // getting angle between hit vector and horizontal axis
-        float newAngle = Vector3.SignedAngle(hitPosition, new Vector3(1,0,0), new Vector3(0,1,0));
+        // getting local position of hit in vector visualization coordinates
+        GameObject vectorVisualization = GameObject.Find("VectorVisualization");
+        Vector3 localHit = vectorVisualization.transform.InverseTransformPoint(hitPosition);
+
+        // getting angle between hit direction and horizontal axis
+        float newAngle = Vector3.SignedAngle(localHit, new Vector3(1,0,0), new Vector3(0,1,0));
 
         // setting the z rotation of the object to this new angle
         objectToRotate.transform.eulerAngles = new Vector3(0,0,newAngle);
@@ -33,8 +37,6 @@ public class RotateObject : MonoBehaviour
         //     // objectToRotate.transform.Rotate(0,0,sign*20f*Time.deltaTime,Space.Self);
 
         //     // get vector from pivot point to hit point
-            
-            
 
         // }   
     }
