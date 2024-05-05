7d885cb88ef12f7248be5ef51752b380ed439cea
simoncwang <wang.c.simon@gmail.com>
2024-05-05 02:38:45 -0400

    completed demo 2 updating, initialized vectors for all demos using new script

--- Assets/Scripts/InfoUpdater.cs
+++ Assets/Scripts/InfoUpdater.cs
@@ -16,6 +16,10 @@ public class InfoUpdater : MonoBehaviour
     void Start()
     {
         Debug.Log("info updater script started");
+        if (DemoName == "demo2") {
+            // translate the position of the square root symbol
+            GameObject.Find("square_root1").transform.Translate(1f,0,0);
+        }
     }
 
     // Update is called once per frame
@@ -28,7 +32,9 @@ public class InfoUpdater : MonoBehaviour
         // getting "real" vectors by multiplying their magnitude by their normalized direction vector
         Vector3 vecOne = vectorOne.transform.right * vectorOneLength;
         Vector3 vecTwo = vectorTwo.transform.right * vectorTwoLength;
-        float dotProduct = Vector3.Dot(vecOne, vecTwo);
+
+        // scaling dot product by scale factor to make sure its accurate
+        float dotProduct = Vector3.Dot(vecOne*scaleFactor, vecTwo*scaleFactor);
 
         // if demo 1 grab text component and update entire block
         if (DemoName == "demo1") {
@@ -59,11 +65,39 @@ public class InfoUpdater : MonoBehaviour
             text += "\na \u2022 b = a<sub>x</sub>b<sub>x</sub> + a<sub>y</sub>b<sub>y</sub>";
 
             text += "\n       = " + (vecTwo.x * scaleFactor).ToString("F1") + " (" + (vecOne.x * scaleFactor).ToString("F1") + ") + " + (vecTwo.y * scaleFactor).ToString("F1") + " (" + (vecOne.y * scaleFactor).ToString("F1") + ")";
-            text += "\n       = -8 + -21 = -29";
+            
+            // calculating products
+            float prodOne = (vecTwo.x * scaleFactor) * (vecOne.x * scaleFactor);
+            float prodTwo = (vecTwo.y * scaleFactor) * (vecOne.y * scaleFactor);   
 
+            text += "\n       = " + prodOne.ToString("F1") + " + " + prodTwo.ToString("F1") + " = " + dotProduct.ToString("F1");
 
+            // setting the new text into the UI
             GameObject.Find("Text (TMP)").GetComponent<TMP_Text>().text = text;
-;
+
+        } else if (DemoName == "demo2"){
+            string text = "";
+
+            text += "Find the magnitude of each vector.";
+
+            // getting + or - depending on sign of coordinate
+            string sign = "";
+            if (vecTwo.y < 0) {
+                sign = "-";
+            } else {
+                sign = "+";
+            }
+
+            text += "\na = " + (vecTwo.x * scaleFactor).ToString("F1") + "i " + sign + Mathf.Abs((vecTwo.y * scaleFactor)).ToString("F1") + "j";
+
+            text += "\n|a| =     a<sub>x</sub><sup>2</sup> + a<sub>y</sub><sup>2</sup>";
+            text += "\n|a| =    " + (vecTwo.x * scaleFactor).ToString("F1") + "<sup>2</sup>" + sign + Mathf.Abs((vecTwo.y * scaleFactor)).ToString("F1") + "<sup>2</sup> =  ";
+            text += "" + ((vecTwo.x * scaleFactor)*(vecTwo.x * scaleFactor)).ToString("F1") + " + " + ((vecTwo.y * scaleFactor)*(vecTwo.y * scaleFactor)).ToString("F1") + " =    ";
+            text += ((vecTwo.x * scaleFactor)*(vecTwo.x * scaleFactor) + (vecTwo.y * scaleFactor)*(vecTwo.y * scaleFactor)).ToString("F1");
+            text += "\n				= " + Mathf.Sqrt((vecTwo.x * scaleFactor)*(vecTwo.x * scaleFactor) + (vecTwo.y * scaleFactor)*(vecTwo.y * scaleFactor)).ToString("F1");
+
+            // setting the new text into the UI
+            GameObject.Find("Text (TMP)").GetComponent<TMP_Text>().text = text;
         }
         
 
